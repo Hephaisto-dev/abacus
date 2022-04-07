79279e81f36773c8f4d1a991fe86026e1b18b4e4
Mathias Filliol <mathias.filliol@epita.fr>
2022-04-07 10:09:24 +0200

    Initial commit

---
+++ Abacus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Abacus
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello, Abacus!");
+            for (int i = 0; i < args.Length; ++i)
+            {
+                Console.WriteLine("argument {0}: {1}", i, args[i]);
+            }
+
+            // Returns an error code of 0, everything went fine!
+            return 0;
+        }
+    }
+}
